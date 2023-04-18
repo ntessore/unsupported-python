0a835b007e78eda3b9494372854e0af93eeaf9cf
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-04-18 22:59:58 +0100

    fix environment variable name

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class UnsupportedPython(RuntimeError):
 
 class BuildCommand(build):
     def run(self):
-        if os.getenv('DISABLE_UNSUPPORTED_PYTHON', None) is None:
+        if os.getenv('ALLOW_UNSUPPORTED_PYTHON', None) is None:
             raise UnsupportedPython('One or more of your installed packages '
                                     'have indicated that they do not support '
                                     'your version of Python ('
