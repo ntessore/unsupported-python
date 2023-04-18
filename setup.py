75705d239d69cb55e046ccb0525e5734d3332225
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-04-18 22:35:11 +0100

    exception at runtime

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+import os
+import platform
 from setuptools import setup
+from setuptools.command.build import build
 
-setup()
+
+class UnsupportedPython(RuntimeError):
+    pass
+
+
+class BuildCommand(build):
+    def run(self):
+        if os.getenv('DISABLE_UNSUPPORTED_PYTHON', None) is None:
+            raise UnsupportedPython('One or more of your installed packages '
+                                    'have indicated that they do not support '
+                                    'your version of Python ('
+                                    + platform.python_version() + ')')
+        return super().run()
+
+
+setup(cmdclass={'build': BuildCommand})
